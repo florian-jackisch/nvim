f5a909dde073e82ed825767ee6358ff729c11e3b
Florian Jackisch <florian.jackisch@gmail.com>
2018-04-25 08:39:55 +0200

    Add helpers to bash source

--- nvim.bash
+++ nvim.bash
@@ -1,3 +1,8 @@
 # Use cquery
 export PATH="$PATH:$HOME/.config/nvim/plugged/cquery/build/release/bin"
 
+# Use nvim instead of vim
+alias vim="nvim"
+alias vi="nvim"
+alias vimdiff='nvim -d'
+export EDITOR=nvim
