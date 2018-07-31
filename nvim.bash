9e717e56521b4596318ea3c81e94f2917afe25c0
Florian Jackisch <florian.jackisch@gmail.com>
2018-07-31 08:15:39 +0200

    Add gruvbox as default themes

--- nvim.bash
+++ nvim.bash
@@ -11,5 +11,5 @@ set keymap vi-command
 # Color scheme
 BASE16_SHELL=$HOME/.config/nvim/plugged/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
-alias darkbg="base16_tomorrow-night"
-alias lightbg="base16_tomorrow"
+alias darkbg="base16_gruvbox-dark-hard"
+alias lightbg="base16_gruvbox-light-hard"
